84955c2701f81d5ba1ea7b5ff8eab8f6d84740c5
YefeiGao <kaofee0429@126.com>
2019-11-15 16:59:44 +0800

    1. use fit_generator with user define data generator

--- examples/run_dsp_ctr.py
+++ examples/run_dsp_ctr.py
@@ -8,17 +8,16 @@ from deepctr.models import DeepFM
 from deepctr.inputs import SparseFeat, VarLenSparseFeat, get_feature_names
 
 
-def split(x):
-  key_ans = x.split(':')
-  for key in key_ans:
-    if key not in key2index:
-      # Notice : input value 0 is a special "padding",so we do not use 0 to encode valid feature for sequence input
-      key2index[key] = len(key2index) + 1
-  return list(map(lambda x: key2index[x], key_ans))
-
-
 if __name__ == "__main__":
-  feat_name_list = ["label"]
+  num_epochs = 1
+  batch_size = 1024
+  max_varlen = 5
+  num_train = 221102858
+  num_test = 2233373
+  train_dir = "../data/dsp_ctr/train/"
+  test_dir = "../data/dsp_ctr/test/"
+
+  feat_name_list = []
   feat_dtype_dict = {"label": str}
   sparse_features = {}
   varlen_features = {}
@@ -39,60 +38,68 @@ if __name__ == "__main__":
         sparse_features[field_name] = int(bucket_size)
         sparse_feat_col_ids[field_name] = int(field_id)
 
-  data = pd.DataFrame(columns=feat_name_list)
-  data_dir = "../data/dsp_ctr/"
-  for parent, _, filenames in os.walk(data_dir):
-    for filename in filenames:
-      data_part = pd.read_table(os.path.join(parent, filename), names=feat_name_list, dtype=feat_dtype_dict, header=None, sep="\t")
-      data = data.append(data_part, ignore_index=True)
 
-  target = ['label']
+  def dsp_ctr_generator(data_dir, bs, mode='train'):
+    for parent, _, filenames in os.walk(data_dir):
+      while True:
+        for filename in filenames:
+          with open(os.path.join(parent, filename), "r") as f:
+            model_input_label = {}
+            model_input_feat = {}
+            cat_features = {}
+            multi_cat_features = {}
+            model_input_label["prediction_layer"] = []
+            for feat in feat_name_list:
+              if feat in sparse_features.keys():
+                cat_features[feat] = []
+              else:
+                multi_cat_features[feat] = []
+            while len(model_input_label["prediction_layer"]) < batch_size:
+              line = f.readline()
+              line = line.strip().split("\t")
+              model_input_label["prediction_layer"].append(int(line[0]))
+              for feat, idx in sparse_feat_col_ids.items():
+                cat_features[feat].append(np.int32(line[idx]))
+              for feat, idx in varlen_feat_col_ids.items():
+                multi_feats = line[idx].split(":")
+                if len(multi_feats) >= max_varlen:
+                  multi_feats = multi_feats[:max_varlen]
+                else:
+                  multi_feats.extend([0]*(max_varlen - len(multi_feats)))
+                multi_cat_features[feat].append(np.array(multi_feats, np.int32))
+            model_input_feat.update(cat_features)
+            model_input_feat.update(multi_cat_features)
+            for feat in feat_name_list:
+              if feat in cat_features.keys():
+                model_input_feat[feat] = np.array(cat_features[feat], np.int32)
+              else:
+                model_input_feat[feat] = np.array(multi_cat_features[feat], np.int32)
+            model_input_label["prediction_layer"] = np.array(model_input_label["prediction_layer"], np.int32)
+            yield (model_input_feat, model_input_label)
 
-  # 1.Label Encoding for sparse features,and process sequence features
-  for feat in sparse_features.keys():
-    lbe = LabelEncoder()
-    data[feat] = lbe.fit_transform(data[feat])
 
-  # preprocess the sequence feature
-  varlen_feat_list_dict = {}
-  varlen_feat_len_dict = {}
-  varlen_feat_max_len_dict = {}
-  key2index_dict = {}
-
-  for feat, feat_id in varlen_feat_col_ids.items():
-    key2index = {}
-    feat_list = list(map(split, data[feat].values))
-    feat_length = np.array(list(map(len, feat_list)))
-    feat_max_len = max(feat_length)
-    feat_list = pad_sequences(feat_list, maxlen=feat_max_len, padding='post', )
-
-    key2index_dict[feat] = key2index
-    varlen_feat_list_dict[feat] = feat_list
-    varlen_feat_len_dict[feat] = feat_length
-    varlen_feat_max_len_dict[feat] = feat_max_len
+  trainGen = dsp_ctr_generator(train_dir, batch_size, mode="train")
+  testGen = dsp_ctr_generator(test_dir, batch_size, mode="train")
 
   # 2.count #unique features for each sparse field and generate feature config for sequence feature
   fixlen_feature_columns = [SparseFeat(feat, bucket_size)
                             for feat, bucket_size in sparse_features.items()]
-  varlen_feature_columns = [VarLenSparseFeat(feat, bucket_size, varlen_feat_max_len_dict[feat], 'mean')
+  varlen_feature_columns = [VarLenSparseFeat(feat, bucket_size, max_varlen, 'mean')
                             for feat, bucket_size in varlen_features.items()]  # Notice : value 0 is for padding for sequence input feature
   linear_feature_columns = fixlen_feature_columns + varlen_feature_columns
   dnn_feature_columns = fixlen_feature_columns + varlen_feature_columns
   feature_names = get_feature_names(linear_feature_columns + dnn_feature_columns)
 
-  # 3.generate input data for model
-  model_input = {}
-  for feat in feature_names:
-    if feat in sparse_features.keys():
-      model_input[feat] = data[feat]
-    elif feat in varlen_features.keys():
-      model_input[feat] = varlen_feat_list_dict[feat]
-    else:
-      raise TypeError("Invalid feature column!")
 
   # 4.Define Model,compile and train
   model = DeepFM(linear_feature_columns, dnn_feature_columns, task='binary')
 
   model.compile("adam", "binary_crossentropy", metrics=['binary_crossentropy'], )
-  history = model.fit(model_input, data[target].values,
-                      batch_size=256, epochs=1, verbose=2, validation_split=0.01, )
+  history = model.fit_generator(trainGen,
+                                steps_per_epoch=num_train // batch_size,
+                                validation_data=testGen,
+                                validation_steps=num_test // batch_size,
+                                max_queue_size=1000,
+                                workers=10,
+                                use_multiprocessing=True,
+                                epochs=num_epochs)
