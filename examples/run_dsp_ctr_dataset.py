96b8ac73f87bf6f562cb15764d85b7e0ba5809fa
YefeiGao <kaofee0429@126.com>
2019-11-19 16:11:54 +0800

    1. 重构

--- examples/run_dsp_ctr_dataset.py
+++ examples/run_dsp_ctr_dataset.py
@@ -1,78 +1,103 @@
-import os, sys
-import tensorflow as tf
 import glob
+import os
+import sys
+
+import argparse
+import tensorflow as tf
+
 sys.path.append("../")
-from tensorflow.python.keras.backend import set_session
-
+from tensorflow.python.keras.backend import set_session, get_session
 from tensorflow.python.keras.callbacks import ModelCheckpoint, TensorBoard
-from deepctr.models import DeepFM
-from deepctr.inputs import SparseFeat, VarLenSparseFeat, get_feature_names
+from deepctr.models import PNN
+from deepctr.inputs import SparseFeat, VarLenSparseFeat
 
 
-if __name__ == "__main__":
-  model_path = '../model/'
-  if not os.path.exists(model_path):
-    os.mkdir(model_path)
-  log_dir = '../logs/'
-  if not os.path.exists(model_path):
-    os.mkdir(log_dir)
-  input_type = "textline"
-  num_epochs = 1
-  batch_size = 1024
-  max_varlen = 5
-  num_train = 221102858
-  num_test = 2233373
-  train_dir = "../data/dsp_ctr/train/"
-  test_dir = "../data/dsp_ctr/test/"
-  train_file_list = glob.glob(os.path.join(train_dir, "*"))
-  test_file_list = glob.glob(os.path.join(test_dir, "*"))
+def parse_args():
+  parser = argparse.ArgumentParser(description='training args')
+  # ----net----#
+  parser.add_argument('--net', dest='net', help='net module', type=str)
 
-  feat_name_list = []
-  feat_dtype_dict = {"label": str}
+  # ----data----#
+  parser.add_argument('--data-type', dest='data_type', help='data type like: textline, tfrecords, etc.',
+                      default='textline', type=str)
+  parser.add_argument('--train-data', dest='train_data', help='training data', default=None, type=str)
+  parser.add_argument('--test-data', dest='test_data', help='test data', default=None, type=str)
+  parser.add_argument('--batch-size', dest='batch_size', help='training mini-batch size', default=128, type=int)
+  parser.add_argument('--num-epoch', dest='num_epoch', help='the number of training epochs', default=None, type=int)
+  parser.add_argument('--train-size', dest='train_size', help='the number of training data', default=None,
+                      required=True, type=int)
+  parser.add_argument('--test-size', dest='test_size', help='the number of test data', default=None, required=True,
+                      type=int)
+
+  # ----model----#
+  parser.add_argument('--model-dir', dest='model_dir', help='path to save model checkpoints', default='./', type=str)
+  parser.add_argument('--log-dir', dest='log_dir', help='path to save log files', default='./log/', type=str)
+
+  # ----others----#
+  parser.add_argument('--feat-config', dest='feat_config', help='feature configure file',
+                      default='../bin/field_feature.txt', type=str)
+  parser.add_argument('--padding-size', dest='padding_size', help='num for padding varlen features', default=5, type=int)
+
+  args = parser.parse_args()
+  return args
+
+
+def get_feature_info(feature_config_file):
   sparse_features = {}
   varlen_features = {}
-  sparse_feat_col_ids = {}
-  varlen_feat_col_ids = {}
-  with open("../bin/field_feature.txt", 'r') as ff:
+  with open(feature_config_file, 'r') as ff:
     for line in ff.readlines():
       field_id, _, field_name, _, bucket_size, feature_class = line.strip().split("\t")
       if bucket_size == "-1":
         bucket_size = 1000
-      feat_name_list.append(field_name)
       if feature_class == "multi-cat":
-        feat_dtype_dict[field_name] = str
-        varlen_features[field_name] = int(bucket_size)
-        varlen_feat_col_ids[field_name] = int(field_id)
+        varlen_features[field_name] = [int(field_id), int(bucket_size)]
       else:
-        feat_dtype_dict[field_name] = str
-        sparse_features[field_name] = int(bucket_size)
-        sparse_feat_col_ids[field_name] = int(field_id)
+        sparse_features[field_name] = [int(field_id), int(bucket_size)]
+  return sparse_features, varlen_features
 
 
+def auc(y_true, y_pred):
+  tf_auc = tf.metrics.auc(y_true, y_pred)[1]
+  get_session().run(tf.local_variables_initializer())
+  return tf_auc
 
-  def parse_example(line, feat_name_list, sparse_feat_col_ids, varlen_feat_col_ids, is_training=True):
-    feature = {}
-    columns = tf.string_split([line], '\t')
+
+def main():
+  args = parse_args()
+  if not os.path.exists(args.model_dir):
+    os.mkdir(args.model_dir)
+  if not os.path.exists(args.log_dir):
+    os.mkdir(args.log_dir)
+
+  train_file_list = glob.glob(os.path.join(args.train_data, "*"))
+  test_file_list = glob.glob(os.path.join(args.test_data, "*"))
+
+  def example_parser(example, sparse_feat_ids, varlen_feat_ids, is_training=True):
+    columns = tf.string_split([example], '\t')
     label = {"prediction_layer": tf.string_to_number([columns.values[0]], out_type=tf.int64)}
-    sparse_features = {feat: tf.string_to_number([columns.values[sparse_feat_col_ids[feat]]], out_type=tf.int64)
-                       for feat in sparse_feat_col_ids.keys()}
-    feature.update(sparse_features)
-    varlen_features = {}
+    feature = {}
+    sparse_feats = {feat: tf.string_to_number([columns.values[sparse_feat_ids[feat][0]]], out_type=tf.int64)
+                    for feat in sparse_feat_ids.keys()}
+    feature.update(sparse_feats)
+    varlen_feats = {}
     multi_cat_feat_padding = tf.zeros([5], tf.int64)
-    for feat, feat_id in varlen_feat_col_ids.items():
-      multi_cat_feat = tf.string_split([columns.values[feat_id]], ":")
+    for feat, feat_id in varlen_feat_ids.items():
+      multi_cat_feat = tf.string_split([columns.values[feat_id[0]]], ":")
       multi_cat_feat = tf.string_to_number(multi_cat_feat.values, out_type=tf.int64)
       multi_cat_feat = tf.concat([multi_cat_feat, multi_cat_feat_padding], 0)[:5]
-      varlen_features[feat] = multi_cat_feat
+      varlen_feats[feat] = multi_cat_feat
 
-    feature.update(varlen_features)
+    feature.update(varlen_feats)
     return feature, label
 
-
-  def get_dataset(files, parse_function, num_parallel_calls=10, batch_size=256):
+  def get_dataset(files, parse_function, sparse_feats, varlen_feats, data_type, num_parallel_calls=10, batch_size=256):
     print('Parsing', files)
-    dataset = tf.data.TextLineDataset(files)
-    dataset = dataset.map(lambda line: parse_function(line, feat_name_list, sparse_feat_col_ids, varlen_feat_col_ids, is_training=True),
+    if data_type == "textline":
+      dataset = tf.data.TextLineDataset(files)
+    elif data_type == "tfrecords":
+      dataset = tf.data.TFRecordDataset(files)
+    dataset = dataset.map(lambda line: parse_function(line, sparse_feats, varlen_feats, is_training=True),
                           num_parallel_calls=num_parallel_calls)
     dataset = dataset.shuffle(buffer_size=batch_size, reshuffle_each_iteration=True)
     try:
@@ -82,17 +107,17 @@ if __name__ == "__main__":
     dataset = dataset.prefetch(batch_size)
     return dataset
 
-  train_dataset = get_dataset(train_file_list, parse_example, batch_size=batch_size)
-  test_dataset = get_dataset(test_file_list, parse_example, batch_size=batch_size)
+  sparse_features, varlen_features = get_feature_info(args.feat_config)
+  train_dataset = get_dataset(train_file_list, example_parser, sparse_features, varlen_features, args.data_type, batch_size=args.batch_size)
+  test_dataset = get_dataset(test_file_list, example_parser, sparse_features, varlen_features, args.data_type, batch_size=args.batch_size)
 
   # 2.count #unique features for each sparse field and generate feature config for sequence feature
-  fixlen_feature_columns = [SparseFeat(feat, bucket_size)
-                            for feat, bucket_size in sparse_features.items()]
-  varlen_feature_columns = [VarLenSparseFeat(feat, bucket_size, max_varlen, 'mean')
-                            for feat, bucket_size in varlen_features.items()]  # Notice : value 0 is for padding for sequence input feature
-  linear_feature_columns = fixlen_feature_columns + varlen_feature_columns
+  fixlen_feature_columns = [SparseFeat(feat, bucket_size[1]) for feat, bucket_size in sparse_features.items()]
+  varlen_feature_columns = [VarLenSparseFeat(feat, bucket_size[1], args.padding_size, 'mean')
+                            for feat, bucket_size in
+                            varlen_features.items()]  # Notice : value 0 is for padding for sequence input feature
+  # linear_feature_columns = fixlen_feature_columns + varlen_feature_columns
   dnn_feature_columns = fixlen_feature_columns + varlen_feature_columns
-  feature_names = get_feature_names(linear_feature_columns + dnn_feature_columns)
 
   config = tf.ConfigProto()
   config.gpu_options.allow_growth = True
@@ -100,19 +125,21 @@ if __name__ == "__main__":
   set_session(tf.Session(config=config))
 
   # 4.Define Model,compile and train
-  model = DeepFM(linear_feature_columns, dnn_feature_columns, task='binary')
+  model = PNN(dnn_feature_columns, use_inner=True, use_outter=True, task='binary')
   model.summary()
-  model.compile("adam", "binary_crossentropy", metrics=['binary_crossentropy'], )
-  # checkpoit = ModelCheckpoint(filepath=os.path.join(model_path, 'model-{epoch:02d}.h5'))
-  checkpoit = ModelCheckpoint(filepath=os.path.join(model_path, 'model-{epoch:02d}.h5'), monitor='val_acc', verbose=1,
-                              save_best_only=True, mode='max')
-  tensorboard = TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=True, write_images=True)
+  model.compile("adam", "binary_crossentropy", metrics=['binary_crossentropy', auc], )
+  checkpoint = ModelCheckpoint(filepath=os.path.join(args.model_dir, 'model-{epoch:02d}.h5'), monitor=auc, verbose=1,
+                               save_best_only=True, mode='max')
+  tensorboard = TensorBoard(log_dir=args.log_dir, histogram_freq=0, write_graph=True, write_images=True)
 
   history = model.fit(train_dataset,
-                      steps_per_epoch=num_train // batch_size,
-                      epochs=num_epochs,
+                      steps_per_epoch=args.train_size // args.batch_size,
+                      epochs=args.num_epoch,
                       verbose=1,
-                      callbacks=[checkpoit, tensorboard],
+                      callbacks=[checkpoint, tensorboard],
                       validation_data=test_dataset,
-                      validation_steps=num_test // batch_size)
+                      validation_steps=args.test_size // args.batch_size)
 
+
+if __name__ == '__main__':
+  main()
--- examples/run_dsp_ctr_dataset.sh
+++ examples/run_dsp_ctr_dataset.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 rm -rf ../log/log_dsp_ctr_dataset_deepfm
-CUDA_VISIBLE_DEVICES="2" /home/gaoyefei/miniconda3/envs/deepctr/bin/python run_dsp_ctr_dataset.py > ../log/log_dsp_ctr_dataset_deepfm 2>&1 &
+CUDA_VISIBLE_DEVICES="2" /home/gaoyefei/miniconda3/envs/deepctr/bin/python run_dsp_ctr_dataset_test.py \
+  --net PNN \
+  --data-type textline \
+  --train-data ../data/dsp_ctr/train \
+  --test-data ../data/dsp_ctr/test \
+  --batch-size 1024 \
+  --num-epoch 1 \
+  --train-size 221102858 \
+  --test-size 2233373 \
+  --model-dir ../model \
+  --log-dir ../log \
+  --feat-config ../bin/field_feature.txt \
+  --padding-size 5 \
+  > ../log/log_dsp_ctr_dataset_deepfm 2>&1 &
